e153f1e3d72e1d9d1452f7db2fc16f951dbf2852
Kxpark <104218928+Kxpark@users.noreply.github.com>
2025-01-28 21:01:04 -0600

    Create GS_Launch.sh

---
+++ handmardGxE/GS_Launch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --export=NONE               # do not export current env to the job
+#SBATCH --job-name=GblupparGXE.hand_noW.4.5.nofixed.LOO      # job name
+#SBATCH --time=03-00:00:00             # max job run time dd-hh:mm:ss
+#SBATCH --ntasks-per-node=1         # tasks (commands) per compute node
+#SBATCH --cpus-per-task=48          # CPUs (threads) per command
+#SBATCH --mem=360GB                  # total memory per node
+#SBATCH --output=stdout.%x.%j       # save stdout to file
+#SBATCH --error=stderr.%x.%j        # save stderr to file
+#SBATCH --mail-user=
+#SBATCH --mail-type=all
+
+
+module purge
+ml GCC/11.2.0  OpenMPI/4.1.1 R_tamu/4.2.0 
+Rscript Gxe.R
